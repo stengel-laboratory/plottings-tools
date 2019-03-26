eafb4e248fe64987f8862f70b73db9e21171b28c
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2019-03-26 17:32:40 +0100

    Add option to write current parameters to config file Change empty default values from empty strings to None Modify help formatter to display description with line breaks

--- src/plot_bag_container.py
+++ src/plot_bag_container.py
@@ -13,17 +13,20 @@ import logging
 # TODO: uid level is fine; however for doing the violations on uxid level they would have to be calculated before sum()
 # TODO: use regular containers as a control
 
-desc = """Kai Kammer - 2018-09-17. 
-Script to plot xTract bag container ms1 areas. All plots are by default saved to the folder 'plots'
+desc = """Kai Kammer - 2018-09-17.\n
+Script to plot xTract bag container ms1 areas. All plots are by default saved to the folder 'plots'.\n
 """
 
-parser = configargparse.ArgParser(description=desc, formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
+class Formatter(configargparse.ArgumentDefaultsHelpFormatter, configargparse.RawDescriptionHelpFormatter): pass
+parser = configargparse.ArgParser(description=desc, formatter_class=Formatter)
 parser.add_argument('input', action="store", default=None, type=str, nargs='+',
                     help="List of input csv files separated by spaces")
-parser.add_argument('-c', '--config_file', required=False, is_config_file=True,
+parser.add_argument('-cf', '--config_file', is_config_file=True,
                     help='Optionally specify a config file containing your settings')
+parser.add_argument('-cw', '--config_write', is_write_out_config_file_arg=True,
+                    help='Optionally specify a file to save your current settings to')
 parser.add_argument('-o', '--outname', action="store", dest="outname", default='plots',
-                    help="Name of the plot folder")
+                    help="Name of the folder to save the plots to")
 parser.add_argument('-l', '--level_ms1', action="store", dest="level", default='uID',
                     help="Level on which the ms1 intensities are summed. Either uID (peptide)"
                          "or uxID (protein).")
@@ -31,7 +34,7 @@ parser.add_argument('-p', '--plot_type', action="store", dest="plot", default='s
                     help="Type of plot. Possible values: scatter, bar,"
                          " lh (light heavy), rep (replicates), rep_bar, cluster, std (standard deviation),"
                          " link (ms1 area overview), log2r (log2ratio), dil (dilution series), domain, dist (distance)")
-parser.add_argument('-f', '--filter', action="store", dest="filter", default="",
+parser.add_argument('-f', '--filter', action="store", dest="filter", default=None,
                     help="Optionally specify a link type to filter for. Possible values: monolink, xlink")
 parser.add_argument('-e', '--sel_exp', action="store_true", dest="sel_exp", default=False,
                     help="Optionally provide this flag to exclude specific experiments before plotting")
@@ -49,24 +52,24 @@ parser.add_argument('-ep', '--experiment_percentage', action="store", dest="expe
 parser.add_argument('-v', '--vio_list', action="store", dest='vio_list', default=['lh', 'xt'], type=str, nargs='+',
                     help="List of input possible violation filters separated by spaces. "
                          "Possible values: lh (light/heavy log2 ratio, xt (xTract type violations), none (no filtering")
-parser.add_argument('-dom', '--domains', action="store", dest="domains", default="",
+parser.add_argument('-dom', '--domains', action="store", dest="domains", default=None,
                     help="Optionally specify a file containing domain ranges to color certain plots.")
-parser.add_argument('-dis', '--distance', action="store", dest="distance", default="",
+parser.add_argument('-dis', '--distance', action="store", dest="distance", default=None,
                     help="Optionally specify a file containing domain ranges to color certain plots.")
-parser.add_argument('-w', '--whitelist', action="store", dest="whitelist", default="",
+parser.add_argument('-w', '--whitelist', action="store", dest="whitelist", default=None,
                     help="Optionally specify a file containing allowed links (uxids), i.e. a whitelist.")
-parser.add_argument('-s', '--sortlist', action="store", dest="sortlist", default="",
+parser.add_argument('-s', '--sortlist', action="store", dest="sortlist", default=None,
                     help="Optionally specify a file containing the order of experiments.")
 args = parser.parse_args()
 
 log_file = '{0}/plot_bag_container.log'.format(args.outname)
 os.makedirs(args.outname, exist_ok=True)
 logging.basicConfig(filename=log_file,level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S:')
-print('\nPlot parameters are written to {0}\n'.format(log_file))
-logging.info('Start plot with the following parameters \n' + parser.format_values())
 
 
 def main():
+    print('\nPlot parameters are written to {0}\n'.format(log_file))
+    logging.info('Start plot with the following parameters \n' + parser.format_values())
     df_list = []  # tuple (dataframe, bag_container)
     df_domains = None
     df_dist = None
